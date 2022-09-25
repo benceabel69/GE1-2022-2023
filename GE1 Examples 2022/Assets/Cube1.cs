98c01c560386a51bcecaf8abdbe90359b8771b96
Bence Ábel <bence.abel@gmail.com>
2022-09-25 23:42:07 +0100

    changed speed from fps to seconds

--- GE1 Examples 2022/Assets/Cube1.cs
+++ GE1 Examples 2022/Assets/Cube1.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Cube1 : MonoBehaviour
 {
+
+
     // Start is called before the first frame update
     void Start()
     {
+       
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0, 1);
+        transform.Translate(0,0, 1 * Time.deltaTime);
+      
     }
 }
